ab6b55ca74fe3a3f6731fccb383e373d134c6f05
jin <znu660@naver.com>
2022-04-06 11:41:04 +0900

    ListView 작성
    Button을 누르면 EditText에 작성된 Text가 ListView에 등록되는 기능을 구현

--- ListView/app/src/main/java/kr/co/company/listview/MainActivity.java
+++ ListView/app/src/main/java/kr/co/company/listview/MainActivity.java
@@ -3,12 +3,49 @@ package kr.co.company.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button btn;
+    private String editTextstr;
+
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView)findViewById(R.id.list);
+        editText =(EditText)findViewById(R.id.editText);
+        btn = (Button)findViewById(R.id.btn);
+
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,data);
+
+        list.setAdapter(adapter);
+
+        data.add("시작");
+        adapter.notifyDataSetChanged();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editTextstr = editText.getText().toString();
+                data.add(editTextstr);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
     }
 }
