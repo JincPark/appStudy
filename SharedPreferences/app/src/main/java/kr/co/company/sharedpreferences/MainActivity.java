305839e17b32c610f014a4ee1de3355549ded662
jin <znu660@naver.com>
2022-04-11 16:48:06 +0900

    임시저장 공부
    에뮬레이터와 실물폰 사이의 차이가 있음. 알아보도록

--- SharedPreferences/app/src/main/java/kr/co/company/sharedpreferences/MainActivity.java
+++ SharedPreferences/app/src/main/java/kr/co/company/sharedpreferences/MainActivity.java
@@ -2,13 +2,41 @@ package kr.co.company.sharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText et_save;
+    String shared = "file";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_save = (EditText) findViewById(R.id.et_save);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+
+        String value = sharedPreferences.getString("save1",""); //onDestroy 에서 임시저장한 값을 어플실행시 불러옴
+        et_save.setText(value);
+
+
+    }
+// 어플을 껐을 때 임시 저장하는 생명주기 onDestroy
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        String value = et_save.getText().toString();
+        editor.putString("save1",value);
+        editor.commit(); // 저장
+
+
+
     }
 }
